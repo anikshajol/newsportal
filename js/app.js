f56f46c7f5273641b4cec4144834d67c6aece7c1
anikshajol <anikshajol@gmail.com>
2022-09-04 23:12:31 +0600

    blog updated responsive issue fixed javascript updated

--- js/app.js
+++ js/app.js
@@ -1,160 +1,184 @@
-// category 
+// category
 
-const loadCategory = ()=>{
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
+const loadCategory = () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
 
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayCategory(data.data))
-    .catch(error => console.log(error))
-}
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCategory(data.data))
+    .catch((error) => console.log(error));
+};
 
-
-
-const displayCategory = (category)=>{
-const menuContent = document.getElementById('menu-content')
-
-console.log(category.news_category);
-
-
-
-category.news_category.forEach(content => {
-    // console.log(content.category_id);
-    const contentLi = document.createElement('li');
-contentLi.classList.add('m-4')
+const displayCategory = (category) => {
+  const menuContent = document.getElementById("menu-content");
+ 
+  category.news_category.forEach((content) => {
+    const contentLi = document.createElement("li");
+    contentLi.classList.add("m-4");
 
     contentLi.innerHTML = `
    
    <a onclick= "loadArticle(${content.category_id})">${content.category_name}</a>
-    `
+    `;
 
-    menuContent.appendChild(contentLi)
+    menuContent.appendChild(contentLi);
+  });
 
-});
+};
 
+const loadArticle = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/0${id}`;
 
-}
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayArticle(data.data))
+    .catch((error) => console.log(error));
+};
 
-
-
-const loadArticle=(id)=>{
-  
-    const url = `https://openapi.programming-hero.com/api/news/category/0${id}`
-    // console.log('load',id);
-    console.log(url);
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayArticle(data.data))
-    .catch(error=>console.log(error))
-   
-}
-
-const displayArticle=(news)=>{
-    const newsContainer = document.getElementById('news-container');
-    newsContainer.textContent = '';
-
-    // load Spinner
-    loadSpinner(true)
-
-// search result
-    const id = document.getElementById('number');
-    const noResult = document.getElementById('no-result-found')
-    
-    if(news.length){
-        noResult.classList.remove('hidden')
-        id.innerText = parseInt(news.length)
-        
-    }else{
-        id.innerText = '0'
-        noResult.classList.remove('hidden')
+// loading spinner
+const loadSpinner = (isLoading) => {
+    const spinner = document.getElementById("spinner");
+    if (isLoading) {
+      spinner.classList.remove("hidden");
+    } else {
+      spinner.classList.add("hidden");
     }
+  };
 
-news.forEach(content => {
-    console.log(content);
-    const {author, thumbnail_url,title,details,total_view,_id}= content
-  const {name,published_date,img} = author
+const displayArticle = (news) => {
+  const newsContainer = document.getElementById("news-container");
 
-    const div = document.createElement('div');
-    div.classList.add("card", "lg:flex", "grid", "grid-cols-1", "lg:card-side", "bg-base-100", "shadow-xl", "mb-4",)
+  newsContainer.textContent = "";
 
-    div.innerHTML=`
+  // load Spinner
+    loadSpinner(true);
+  
+
+  // search result
+  const id = document.getElementById("number");
+  const noResult = document.getElementById("no-result-found");
+
+
+  if (news.length) {
+    noResult.classList.remove("hidden");
+    id.innerText = parseInt(news.length);
+  } else {
+    id.innerText = "0";
+    noResult.classList.remove("hidden");
+  }
+
+  news.forEach((content) => {
+    const { author, thumbnail_url, title, details, total_view, _id } = content;
+    const { name, published_date, img } = author;
+
+    const div = document.createElement("div");
+    div.classList.add(
+      "card",
+      "lg:flex",
+      "grid",
+      "grid-cols-1",
+      "lg:card-side",
+      "bg-base-100",
+      "shadow-xl",
+      "mb-4"
+    );
+
+    div.innerHTML = `
     <figure>
         <img src="${thumbnail_url}" class="" alt="Album">
     </figure>
     <div class="card-body">
             <h2 class="card-title">${title}</h2>
-            <p>${details.length>20?details.slice(0,20)+'...':details}</p>
+            <p>${
+              details.length > 20 ? details.slice(0, 20) + "..." : details
+            }</p>
         <div class="card-actions justify-between">
             <section class= "flex gap-5" >
             <figure>
                 <img src="${img}" class="w-10 rounded-full" alt="" />
             </figure>
             <div>
-                <p>${name}</p>
-                <p>${published_date}</p>
+                <p>${name === null
+                    ? "No Data Found"
+                    : name === "system"
+                    ? "No Data Found"
+                    : name}</p>
+                <p>${published_date === null
+                    ? "No Data Found"
+                    : published_date === "system"
+                    ? "No Data Found"
+                    : published_date}</p>
             </div>
             </section>
             <div>
-                <p><span><i class="fa-regular fa-eye"></i></span> ${total_view}</p>
+                <p><span><i class="fa-regular fa-eye"></i></span> ${total_view == null
+                    ? "No Data Found"
+                    : total_view === "system"
+                    ? "No Data Found"
+                    : total_view}</p>
             </div>
             <label onclick="newsDetails('${_id}')" for="my-modal-3" class="btn modal-button">Show Details</label>
         </div>
     </div>
     
-    `
-    newsContainer.appendChild(div)
+    `;
+    newsContainer.appendChild(div);
+  });
 
-    
-   
-});
-// stop spinner
-loadSpinner(false)
+ // stop spinner
+loadSpinner(false);
+  
+};
 
-}
-
-// loading spinner
-const loadSpinner = isLoading =>{
-    const spinner = document.getElementById('spinner')
-    if(isLoading){
-        spinner.classList.remove('hidden')
-    }else{
-        spinner.classList.add('hidden')        
-    }
-}
 
 
 // Modal
 
-const newsDetails = (newsId) =>{
-    const url = (`https://openapi.programming-hero.com/api/news/${newsId}`)
-    fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayNewsDetails(data.data[0]))
-    .catch(error=>console.log(error))    
-}
+const newsDetails = (newsId) => {
+  const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayNewsDetails(data.data[0]))
+    .catch((error) => console.log(error));
+};
 
+const displayNewsDetails = (newsDetails) => {
+  const { author, details, image_url, title, total_view } = newsDetails;
 
+  const { name, img, published_date } = author;
 
-const displayNewsDetails= (newsDetails)=>{
+  const modalBody = document.getElementById("modal-body");
 
-const {author,details,image_url,title,total_view}=newsDetails;
-
-const {name,img,published_date}=author;
-
-const modalBody = document.getElementById('modal-body')
-
-modalBody.innerHTML=` 
+  modalBody.innerHTML = ` 
 <figure><img src="${image_url}" alt="Shoes" class= 'p-4'  /></figure>
 <section class= "flex gap-5 mb-5" >
             <figure>
                 <img src="${img}" class="w-10 rounded-full" alt="" />
             </figure>
             <div>
-                <p>${name===null ?"No Data Found" :name==="system"?"No Data Found":name}</p>
-                <p>${published_date===null?"No Data Found":published_date==="system"?"No Data Found":published_date}</p>
+                <p>${
+                  name === null
+                    ? "No Data Found"
+                    : name === "system"
+                    ? "No Data Found"
+                    : name
+                }</p>
+                <p>${
+                  published_date === null
+                    ? "No Data Found"
+                    : published_date === "system"
+                    ? "No Data Found"
+                    : published_date
+                }</p>
             </div>
             <div>
-            <p><span><i class="fa-regular fa-eye"></i></span> ${total_view==null?'No Data Found':total_view==="system"?"No Data Found" :total_view}</p>
+            <p><span><i class="fa-regular fa-eye"></i></span> ${
+              total_view == null
+                ? "No Data Found"
+                : total_view === "system"
+                ? "No Data Found"
+                : total_view
+            }</p>
         </div>
 </section>
            
@@ -162,10 +186,11 @@ modalBody.innerHTML=`
 <p>${details}</p>
 
 
-`
-}
+`;
 
-// newsDetails()
+};
 
-loadCategory()
-loadArticle(01)
+
+loadCategory();
+loadArticle(01);
+
